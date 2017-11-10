c622ab187e049d9bf8c0709af5f93fd96c81b7ee
tsocheezy <tettehmichael300@gmail.com>
2017-11-10 03:18:27 +0000

    interceptor fixed

--- src/main/java/controllers/access/LoginAction.java
+++ src/main/java/controllers/access/LoginAction.java
@@ -5,51 +5,70 @@ import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
 import com.opensymphony.xwork2.ModelDriven;
 import entities.users.User;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.struts2.ServletActionContext;
 import org.apache.struts2.interceptor.SessionAware;
 import utilities.Constants;
 
+import javax.servlet.http.HttpSession;
 import java.util.Map;
-import java.util.Optional;
 
 
-public class LoginAction extends ActionSupport implements SessionAware, Constants, ModelDriven<User> {
+public class LoginAction extends ActionSupport implements Constants, SessionAware, ModelDriven<User> {
 
-    @Getter
-    @Setter
-    private User user;
-    private Map<String, Object> userSession;
-    private boolean loggedIn = false;
+    private static final long serialVersionUID = 1L;
+
+    private Map<String, Object> session = null;
+    private User user = new User();
+
+    private static final Logger logger = LogManager.getLogger(LoginAction.class);
 
     main.BikeRentalSingleton bikeRentalSingleton = main.BikeRentalSingleton.getInstance();
 
 
     @Override
     public String execute() throws Exception {
-            if (bikeRentalSingleton.getUserModel()
-                    .login(user.getUsername(), user.getPassword())) {
+        // check for user session existence
+        HttpSession currentSession = ServletActionContext.getRequest().getSession(true);
+        // cast object to user object
+        User getSessionAttr = (User) currentSession.getAttribute(LOGGED_IN_USER);
 
-                // store logged user in session
-                userSession = ActionContext.getContext().getSession();
-                userSession.put(LOGGED_IN_USER, user);
-                loggedIn = true;
+        String action = null;
 
-                return "dashboard";
-            }
+        if (getSessionAttr != null) {
+            action = SUCCESS;
+        } else if (user.getUsername() == null || user.getPassword() == null) {
+            action = INPUT;
 
+        } else if (user.getUsername().isEmpty() || user.getPassword().isEmpty()) {
+            addActionError("Username/Password cannot be left blank");
 
-        return "login";
+        } else if (bikeRentalSingleton.getUserModel().login(user.getUsername(), user.getPassword())) {
+            // store logged user in session
+            session.put(LOGGED_IN_USER, user);
+            action = SUCCESS;
+        }
+
+        return action;
     }
 
     @Override
-    public void setSession(Map<String, Object> userSession) {
-        this.userSession = userSession;
+    public void setSession(Map<String, Object> map) {
+        this.session = map;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
     }
 
     @Override
     public User getModel() {
         return user;
     }
+
 }
